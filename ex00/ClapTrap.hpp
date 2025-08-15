d9c59e4f41a6147c7a150aaef8474a324ee7b7c2
Natalia Medeiros Da Silva De Almeida <nmedeiro@f1r7s2.codam.nl>
2025-08-15 10:07:07 +0200

    implemented ClapTrap class

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,11 +6,29 @@
 /*   By: nmedeiro <nmedeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 09:59:33 by nmedeiro          #+#    #+#             */
-/*   Updated: 2025/08/15 09:59:34 by nmedeiro         ###   ########.fr       */
+/*   Updated: 2025/08/15 10:06:31 by nmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
+
+class ClapTrap
+{
+    private:
+        strd::string    name;
+        int             hitPoints;
+        int             energypoints;
+        int             attackDamage;
+    public:
+        ClapTrap();
+        ClapTrap(const ClapTrap &clapTrap);
+        ClapTrap& operator=(const ClapTrap &clapTrap);
+        ~ClapTrap();
+        
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+};
 
 #endif
