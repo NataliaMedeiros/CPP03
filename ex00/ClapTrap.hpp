037408385d20aebf0c86be976a802d5128fdbab9
Natália Medeiros Da Silva De Almeida <natalia@MacBookPro.home>
2025-10-08 12:44:04 +0200

    finished ex00 and started ex01

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: natalia <natalia@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/10/06 09:31:23 by natalia       #+#    #+#                 */
-/*   Updated: 2025/10/08 11:48:47 by natalia       ########   odam.nl         */
+/*   Updated: 2025/10/08 12:43:18 by natalia       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,6 @@ public:
 	ClapTrap(const ClapTrap &clapTrap); //Copy constructor
 	ClapTrap& operator=(const ClapTrap &clapTrap); //Copy assignment operator
 	~ClapTrap();//Destructor
-
-	// std::string getName() const;
-	// int getHitPoints() const;
-	// int getEnergyPoints() const;
-	// int getAttackDamage() const;
-
-	// void setHitPoints(int const value);
-	// void setEnergyPoints(int const value);
-	// void setAttackDamage(int const value);
 
 	void attack(const std::string& target);
 	void takeDamage(unsigned int amount);
