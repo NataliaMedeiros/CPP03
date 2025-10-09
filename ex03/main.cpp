57e81a55d6e6676b18c4fe867b6603ddf6842a2f
Natália Medeiros Da Silva De Almeida <natalia@MacBookPro.home>
2025-10-09 15:37:10 +0200

    working on main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: natalia <natalia@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/10/09 10:13:26 by natalia       #+#    #+#                 */
-/*   Updated: 2025/10/09 15:27:25 by natalia       ########   odam.nl         */
+/*   Updated: 2025/10/09 15:36:34 by natalia       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,43 @@
 
 int main(void)
 {
-    ClapTrap clapper("CLAPPER");
-	ClapTrap Ninja("NINJA ASSASSIN");
-    ScavTrap scavver("SCAVVER");
-    FragTrap fragger("FRAGGER");
-    DiamondTrap diamondder("DIAMONDDER");
+    ClapTrap clap("clap");
+	// ClapTrap Ninja("NINJA ASSASSIN");
+    // ScavTrap scavver("SCAVVER");
+    // FragTrap fragger("FRAGGER");
+    // DiamondTrap diamondder("DIAMONDDER");
 
-	clapper.takeDamage(5);
-    clapper.beRepaired(1);
-    clapper.attack("NINJA ASSASSIN");
-	clapper.takeDamage(20);
-    clapper.beRepaired(1);
+	clap.takeDamage(5);
+    clap.beRepaired(1);
+    clap.attack("anti-clap");
+	clap.takeDamage(20);
+    clap.beRepaired(1);
 
-    scavver.attack("NINJA ASSASSIN");
-    scavver.takeDamage(5);
-    scavver.beRepaired(1);
-    scavver.guardGate();
-    scavver.takeDamage(9);
-    scavver.attack("NINJA ASSASSIN");
-    scavver.takeDamage(105);
-    scavver.beRepaired(1);
-    scavver.guardGate();
+    // scavver.attack("NINJA ASSASSIN");
+    // scavver.takeDamage(5);
+    // scavver.beRepaired(1);
+    // scavver.guardGate();
+    // scavver.takeDamage(9);
+    // scavver.attack("NINJA ASSASSIN");
+    // scavver.takeDamage(105);
+    // scavver.beRepaired(1);
+    // scavver.guardGate();
 
-    fragger.attack("NINJA ASSASSIN");
-    fragger.takeDamage(50);
-    fragger.beRepaired(11);
-    fragger.highFivesGuys();
-    fragger.takeDamage(90);
-    fragger.attack("NINJA ASSASSIN");
-    fragger.highFivesGuys();
+    // fragger.attack("NINJA ASSASSIN");
+    // fragger.takeDamage(50);
+    // fragger.beRepaired(11);
+    // fragger.highFivesGuys();
+    // fragger.takeDamage(90);
+    // fragger.attack("NINJA ASSASSIN");
+    // fragger.highFivesGuys();
 
-    diamondder.attack("NINJA ASSASSIN");
-    diamondder.takeDamage(50);
-    diamondder.beRepaired(11);
-    diamondder.whoAmI();
-    diamondder.takeDamage(90);
-    diamondder.attack("NINJA ASSASSIN");
-    diamondder.whoAmI();
+    // diamondder.attack("NINJA ASSASSIN");
+    // diamondder.takeDamage(50);
+    // diamondder.beRepaired(11);
+    // diamondder.whoAmI();
+    // diamondder.takeDamage(90);
+    // diamondder.attack("NINJA ASSASSIN");
+    // diamondder.whoAmI();
 
 
     return (0);
